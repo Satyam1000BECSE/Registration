b7040aa6eed8874a1c7d51e19daa4561b84e4b82
Satyam1000BECSE <128610133+Satyam1000BECSE@users.noreply.github.com>
2024-09-14 13:39:56 +0530

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -11,7 +11,7 @@ function Login(){
     
     const handleSubmit = (e) => {
         e.preventDefault()
-        axios.post('registration-m6kr.vercel.app/login', {email, password})
+        axios.post('https://registration-m6kr.vercel.app/login', {email, password})
         .then(result => {
             console.log(result)
             if(result.data === "Success"){
